19e638882d38f024f75172ce2dd7d6ba6407b834
Bazzer218 <bzeng24@cmc.edu>
2023-05-02 15:42:31 -0700

    latest

---
+++ alternative_reduce.py
@@ -0,0 +1,50 @@
+#imports
+import matplotlib
+import numpy as np
+import json
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_dir',required=True)
+parser.add_argument('--keys', nargs='+', required=True)
+args = parser.parse_args()
+
+from collections import Counter, defaultdict
+from glob import glob
+
+import os
+import json
+from collections import Counter,defaultdict
+
+input_files = glob(args.input_dir + '/*')
+
+#load each key
+for key in args.keys:
+    yaxis =[]
+    total = defaultdict(lambda: Counter())
+    for path in sorted(input_files):
+        with open(path) as f:
+            tmp = json.load(f) 
+            sumofnum = 0
+            try:
+                for k in tmp[key]:
+                    sumofnum += tmp[key][k]
+            except:
+                pass
+            yaxis.append(sumofnum)
+
+#visualization
+    plt.plot(np.arange(len(yaxis)), yaxis, label=key)
+
+plt.xlabel("Date(2020)")
+plt.ylabel("Number of Tweets")
+plt.title("Tweets Per Hashtag(2020)")
+plt.legend()
+plt.xticks([0, 60, 121, 182, 244, 305], ["1", "3", "5", "7", "9", "11"])
+plt.savefig("lineplot.png")
+
+
+
+
+
