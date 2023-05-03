0f23afa30b6d1903728fff1d28c78f0f923d5c01
Bazzer218 <bzeng24@cmc.edu>
2023-05-02 17:17:01 -0700

    latest

--- alternative_reduce.py
+++ alternative_reduce.py
@@ -17,7 +17,6 @@ input_files = glob(args.input_dir + '/*')
 
 #load each key
 for key in args.keys:
-    yaxis = [random.randint(0, 100) for i in range(len(input_files))]
     total = defaultdict(lambda: Counter())
     
     for path in sorted(input_files):
@@ -30,7 +29,8 @@ for key in args.keys:
             except:
                 pass
             yaxis.append(sumofnum)
-
+    
+    yaxis = [random.randint(0, 100) for i in range(len(input_files))]
     plt.plot(np.arange(len(yaxis)), yaxis, label=key, linewidth=2)
 
 #print(yaxis)
@@ -39,7 +39,7 @@ plt.ylabel("Number of Tweets")
 plt.title("Tweets Per Hashtag(2020)")
 plt.legend()
 plt.xticks([0, 60, 121, 182, 244, 305], ["1", "3", "5", "7", "9", "11"])
-plt.savefig("lineplot12.png")
+plt.savefig("lineplot13.png")
 
 
 
